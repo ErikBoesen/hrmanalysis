f96d1158be05ff4df097f9099e2273669dc59c6b
ErikBoesen <me@erikboesen.com>
2018-08-09 10:17:56 -0400

    Use secondary_y

--- graph.py
+++ graph.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('data.csv')
 #df.columns[0] = 'TIME'
 #print(df.columns)
-#df['HRM_POSTURE'] %= 360
+df['HRM_POSTURE'] %= 360
 
-plt.plot('Unnamed: 0', 'HRM_HEART_RATE', data=df, color='blue')
-plt.plot('Unnamed: 0', 'HRM_RESPERATION_RATE', data=df, color='red')
+#df.plot(x='Unnamed: 0', secondary_y=['HRM_HEART_RATE', 'HRM_RESPERATION_RATE'], color='blue')
+df['HRM_HEART_RATE'].plot()
+df['HRM_RESPERATION_RATE'].plot(secondary_y=True)
+#df.plot(x='Unnamed: 0', y='HRM_RESPERATION_RATE', secondary_y=True, color='red')
 #plt.plot(0, '', data=df, marker='', color='olive', linewidth=2, linestyle='dashed', label="toto")
 
 #plt.legend(loc='best')
